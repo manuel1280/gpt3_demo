7c894e6fe56a28e59b74e574026de8f687024578
manuel1280 <komodocr7@gmail.com>
2022-01-22 09:48:38 -0500

    login views created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  devise_for :users, controllers: {registrations: 'registrations'}
+  devise_for :users
   resources :tickets
-  root "tickets#index"
+  resources :home, only: [:index]
+  root "home#index"
 end
