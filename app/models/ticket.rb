f09b7d10831ce4877042de04c452dad54c5a2851
manuel1280 <komodocr7@gmail.com>
2022-01-22 18:34:12 -0500

    gpt3 library created

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,14 @@
 class Ticket < ApplicationRecord
+  require 'Gpt3'
   belongs_to :user
   validates :body, presence: true
+
+  before_save :classify
+
+  private
+
+  def classify
+    label = Gpt3.classify(body)
+    self.category = label
+  end
 end
---
+++ lib/gpt3.rb
@@ -0,0 +1,28 @@
+class Gpt3
+  require "ruby/openai"
+
+  def self.classify(message)
+    response = client.classifications(parameters: {
+        examples: [
+            ["My laptop does not work", "IT"],["the office internet is dead", "IT"],
+        ["Delivery for client #111 is lost", "Sales"],
+        ["The monthly revenue report is wrong, total income is under the average", "Administration"],
+        ["the web platform is too slow", "IT"],
+        ["I forgot my user credentials", "IT"],
+        ["Many customers has been reporting problems with the quality of our new product", "Marketing"],
+        ["Hola, tengo un problema con las facturas de ventas, se están creado con el valor de deducciones incorrecto", "Accounting"],
+        ["Perdí las llaves de la bodega de suministros, necesito unas nuevas", 'Logistics']
+        ],
+        query: message,
+        model: "davinci"
+    })
+    response['label']
+  end
+
+  private
+  def self.client
+    token = (Rails.env == 'production' ? ENV['OPEN_AI_KEY'] : Rails.application.credentials.open_ai[:key])
+    OpenAI::Client.new(access_token: token)
+  end
+
+end
