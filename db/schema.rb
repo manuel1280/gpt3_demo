47a444460736da7048606119e90eea6017e4c692
johan buitrago <jbuitrago@pibox.app>
2022-08-05 00:32:29 -0500

    fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 2022_08_04_055859) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
-    t.string "AddNameToUsers"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
