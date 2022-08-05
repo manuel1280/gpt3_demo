18900537c838b5a13fde2988bcb9052490195c04
johan buitrago <jbuitrago@pibox.app>
2022-08-05 02:20:33 -0500

    refactor styles and create admin user

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,30 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+
+            // Get the target from the "data-target" attribute
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+
+            if($target.classList.value.includes('is-active')){
+                $target.classList.remove('is-active');
+                console.log('deactive')
+            }else{
+                $target.classList.add('is-active');
+                console.log('active')
+            }
+        });
+    });
+
+});
